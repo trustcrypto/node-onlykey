cdfb5389cd02954964f449f838798b0db782977a
Bradley Matusiak <bmatusiak@gmail.com>
2021-09-11 01:51:53 +0200

    update

--- docs/server.js
+++ docs/server.js
@@ -8,9 +8,9 @@ var server = http.createServer(app);
 
 app.use("/dist", express.static(__dirname + "/../dist"));
 app.use(express.static(__dirname));
-app.use("/libs", express.static(__dirname + "/../libs"));
+// app.use("/libs", express.static(__dirname + "/../libs"));
 
-
+process.env.PORT = process.env.PORT || 3000;
 
 server.listen(process.env.PORT, () => {
     console.log('listening on *:' + process.env.PORT);
